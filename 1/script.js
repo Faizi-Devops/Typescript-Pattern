044cb61059d6273d04572a97ca0221e188f77bd9
Faizan Ali <devfaizi435@gmail.com>
2022-10-12 02:32:55 -0700

    pattern

---
+++ 1/script.js
@@ -0,0 +1,9 @@
+var Hello = function () {
+    for (var i = 1; i <= 5; i++) {
+        for (var j = 1; j <= 5; j++) {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+};
+Hello();
---
+++ 1/script.ts
@@ -0,0 +1,14 @@
+const Hello = () =>{
+    for(let i:number=1;i<=5;i++){
+        for(let j:number=1;j<=5;j++){
+            document.write("*")
+        }
+        document.write("<br>")
+    }
+
+
+
+
+}
+
+Hello();
