80eee7f979b877342561142f6bf9feca3b329775
Faizan Ali <devfaizi435@gmail.com>
2022-10-13 04:16:45 -0700

    pattern

---
+++ 11/script.ts
@@ -0,0 +1,16 @@
+const Hello = () =>{
+    for(let i:number=1;i<=5;i++){
+        for(let j:number=1;j<=i;j++){
+            document.write(`${i}`)
+        }
+        document.write("<br>")
+    }
+
+
+
+
+
+
+
+}
+Hello()
