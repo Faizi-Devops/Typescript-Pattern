9fb8c2271669b846329db9eb20c9609f4019bb12
Faizan Ali <devfaizi435@gmail.com>
2022-10-13 04:16:13 -0700

    pattern

---
+++ 10/script.js
@@ -0,0 +1,9 @@
+var Hello = function () {
+    for (var i = 1; i <= 5; i++) {
+        for (var j = 1; j <= i; j++) {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+};
+Hello();
