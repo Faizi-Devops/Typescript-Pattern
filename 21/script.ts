c56ecfe0f8e9732f8ab55e37d46834d046d0a8c7
Faizan Ali <devfaizi435@gmail.com>
2022-10-14 23:22:02 -0700

    pattern

---
+++ 21/script.ts
@@ -0,0 +1,16 @@
+const Hello = () =>{
+    for(let i:number=1;i<=5;i++){
+        for(let j:number=1;j<=6-i;j++){
+            let newNum:string=String.fromCharCode(j+64);
+            document.write(`${newNum}`)
+        }
+        document.write("<br>")
+    }
+
+
+
+
+
+
+}
+Hello()
