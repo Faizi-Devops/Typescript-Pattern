2df23d3e55a1fb880cd689d73fada0d7accc3bd7
Faizan Ali <devfaizi435@gmail.com>
2022-10-15 01:06:02 -0700

    pattern

---
+++ 27/script.js
@@ -0,0 +1,12 @@
+var Hello = function () {
+    for (var i = 1; i <= 5; i++) {
+        for (var j = 1; j <= 5 - i; j++) {
+            document.write("&nbsp&nbsp");
+        }
+        for (var k = 1; k <= i; k++) {
+            document.write("".concat(k));
+        }
+        document.write("<br>");
+    }
+};
+Hello();
---
+++ 27/script.ts
@@ -0,0 +1,21 @@
+const Hello = () =>{
+    for(let i:number=1;i<=5;i++){
+        for(let j:number=1;j<=5-i;j++){
+            document.write("&nbsp&nbsp")
+        }
+       for(let k:number=1;k<=i;k++){
+        document.write(`${k}`)
+       }
+       document.write("<br>")
+    }
+
+
+
+
+
+
+
+
+
+}
+Hello()
